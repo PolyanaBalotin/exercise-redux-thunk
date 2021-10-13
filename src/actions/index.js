24515e855919a6e3afed41165f53ede20611d47f
Polyana Balotin <polybalotin@hotmail.com>
2021-10-13 19:47:19 -0300

    exercicio 1 - fotos de gatinhos

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,4 +7,11 @@ export const getPicture = (data) => ({ type: GET_PICTURE, data });
 
 export function fetchAPI() {
   // Desenvolva aqui o código da action assíncrona
+  return async (dispatch) => {
+    dispatch(requestAPI());
+    const response = await fetch('https://aws.random.cat/meow')
+    const data = await response.json();
+    return dispatch(getPicture(data))
+    
+  }
 }
